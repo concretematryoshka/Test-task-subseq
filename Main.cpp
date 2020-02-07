1f9c1076c8358b55397910d67505f42d3aa483e2
concretematryoshka <60777354+concretematryoshka@users.noreply.github.com>
2020-02-07 19:42:02 +0300

    Templates added

--- Main.cpp
+++ Main.cpp
@@ -6,19 +6,21 @@
 #include <utility>
 #include <exception>
 
+template<class InputIt>
 struct length_iters
 {
 	length_iters() : substrLenght{ 0 }, secondElem1stOccurIt{}, substrEndIt{}{}
-	length_iters(unsigned int substrLenght, std::string::iterator secondElem1stOccurIt, std::string::iterator substrEndIt) : substrLenght{ substrLenght }, secondElem1stOccurIt{ secondElem1stOccurIt }, substrEndIt{ substrEndIt }{}
+	length_iters(unsigned int substrLenght, InputIt secondElem1stOccurIt, InputIt substrEndIt) : substrLenght{ substrLenght }, secondElem1stOccurIt{ secondElem1stOccurIt }, substrEndIt{ substrEndIt }{}
 
 	unsigned int substrLenght;
-	std::string::iterator secondElem1stOccurIt;
-	std::string::iterator substrEndIt;
+	InputIt secondElem1stOccurIt;
+	InputIt substrEndIt;
 };
 
-length_iters get_length_iters(std::string::iterator iter, std::string::iterator end, const int numb)
+template<class InputIt>
+length_iters<InputIt> get_length_iters(InputIt iter, InputIt end, const int numb)
 {
-	length_iters result{};
+	length_iters<InputIt> result{};
 	result.secondElem1stOccurIt = iter;
 	std::set<char> buffer{};
 	while (iter != end)
@@ -42,7 +44,8 @@ length_iters get_length_iters(std::string::iterator iter, std::string::iterator 
 	result.substrEndIt = iter;
 	return result;
 }
-std::string get_substring(std::string::iterator begin, std::string::iterator end, const int numb)
+template<class ResultValue, class InputIt>
+ResultValue get_substring(InputIt begin, InputIt end, const int numb)
 {
 	if (begin > end || numb < 1)
 		throw std::runtime_error("Wrong arguments\n");
@@ -57,77 +60,34 @@ std::string get_substring(std::string::iterator begin, std::string::iterator end
 			result = buffer;
 		iter = buffer.secondElem1stOccurIt;
 	}
-	return std::string(result.substrEndIt - result.substrLenght, result.substrEndIt);
+	return ResultValue(result.substrEndIt - result.substrLenght, result.substrEndIt);
 }
-
-
-
-std::pair<int, std::pair<std::string::iterator, std::string::iterator>> makepair_sum_iter_(std::string::iterator iter, std::string::iterator end, const int numb)
-{
-	int length{};
-	auto buffer_iter = iter;
-	std::set<char> buffer{};
-	while (iter != end)
-	{
-		auto ok = buffer.insert(*iter);
-		++length;
-		if (ok.second)
-		{
-			if (buffer.size() == 2)
-				buffer_iter = iter;
-			if (buffer.size() > numb)
-			{
-				buffer.erase(*iter);
-				--length;
-				break;
-			}
-		}
-		++iter;
-
-	}
-	return std::make_pair(length, std::make_pair(buffer_iter, iter));
-}
-std::string get_substring_(std::string::iterator begin, std::string::iterator end, const int numb)
-{
-	if (begin > end || numb < 1)
-		throw std::runtime_error("Wrong arguments\n");
-
-	auto result = makepair_sum_iter_(begin, end, numb);
-	auto iter = result.second.first;
-
-	while (iter != end && std::distance(iter, end) > result.first)
-	{ 
-		auto buffer = makepair_sum_iter_(iter, end, numb);
-		if (result.first < buffer.first)
-			result = buffer;
-		iter = buffer.second.first;
-	}
-	return std::string (result.second.second-result.first, result.second.second);
-}
-
 
 void test()
 {
+	
 	std::vector<std::pair<std::string, std::string>> srs_substr_2elem = { std::make_pair("aaabbbdaaabb", "aaabbb"), std::make_pair("aaabbbaaabb", "aaabbbaaabb"), std::make_pair("abcd", "ab"), std::make_pair("abcdd", "cdd"),
 	std::make_pair("aabcdd", "aab"), std::make_pair("abababdabababab", "abababab") };
 	for (auto elem : srs_substr_2elem)
-	{
-		std::cout << elem.second << '\t' << get_substring(elem.first.begin(), elem.first.end(), 2) << std::endl;
-		assert(get_substring(elem.first.begin(), elem.first.end(), 2) == elem.second);
-	}
+		assert(get_substring<std::string>(elem.first.begin(), elem.first.end(), 2) == elem.second);
 
 	std::vector<std::pair<std::string, std::string>> srs_substr_1elem = { std::make_pair("aaabbbdaaabb", "aaa"), std::make_pair("aaabbbaaabbbb", "bbbb"), std::make_pair("abcd", "a"), std::make_pair("abcdd", "dd"),
 	std::make_pair("aabcdd", "aa"), std::make_pair("abababdabababab", "a"), std::make_pair("a", "a") };
 	for (auto elem : srs_substr_1elem)
-		assert(get_substring(elem.first.begin(), elem.first.end(), 1) == elem.second);
+		assert(get_substring<std::string>(elem.first.begin(), elem.first.end(), 1) == elem.second);
 
 	std::vector<std::pair<std::string, std::string>> srs_substr_3elem = { std::make_pair("aaabbbdaaabb", "aaabbbdaaabb"), std::make_pair("aaabbbadfaabbbb", "aaabbbad"), std::make_pair("abcd", "abc"), std::make_pair("abcdd", "bcdd"),
 	std::make_pair("aabcdd", "aabc"), std::make_pair("a", "a") };
 	for (auto elem : srs_substr_3elem)
-		assert(get_substring(elem.first.begin(), elem.first.end(), 3) == elem.second);
+		assert(get_substring<std::string>(elem.first.begin(), elem.first.end(), 3) == elem.second);
 
 	for (auto elem : srs_substr_3elem)
-		assert(get_substring(elem.first.begin(), elem.first.end(), 4) == elem.first);
+		assert(get_substring<std::string>(elem.first.begin(), elem.first.end(), 4) == elem.first);
+
+	std::vector <std::vector<int>> srs_numeric = { { 1,1,1,2,2,2,4,1,1,1,2,2 }, {1,1,1,2,2,2,1,1,1,2,2}, {1,2,3,4}, {1,2,3,4,44,4,44}, {0, -5, 0, 5, -5} };
+	std::vector <std::vector<int>> res_numeric = { { 1,1,1,2,2,2 }, {1,1,1,2,2,2,1,1,1,2,2}, {1,2}, {4,44,4,44}, {0, -5, 0} };
+	for(int i = 0; i< srs_numeric.size(); ++i)
+		assert(get_substring<std::vector<int>>(srs_numeric[i].begin(), srs_numeric[i].end(), 2) == res_numeric[i]);
 }
 
 
